bf467cbfc99028673d424576ef3f5a806f6d465c
Ciallo <dison2233@126.com>
2025-09-04 17:34:49 +0800

    new listener

--- listener/CRecipientFilter.hpp
+++ listener/CRecipientFilter.hpp
@@ -1,4 +1,4 @@
-virtual uint64_t unk_65a575ee0c6e5674() = 0;
-virtual uint64_t unk_bad_sig() = 0;
-virtual uint64_t unk_6fb90426e78808d5() = 0;
-virtual uint64_t unk_bad_sig() = 0;
+virtual void bc15c044d88254eb() = 0;
+virtual void 6fb90426e78808d5() = 0;
+virtual void 6fb90426e78808d5() = 0;
+virtual void a368c7ac3913d0cb() = 0;
